d14ae2b9fe761446ebef43a5644eb107c3761842
rhazes <rhazes@shutterstock.com>
2018-12-01 21:25:01 -0500

    Add basic components and empty containers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Sidebar from './Components/Sidebar/Sidebar';
+import Messages from './Components/Main/Messages';
+import MessageInput from './Components/Main/MessageInput';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Sidebar />
+        <div className="chatMain">
+          <Messages />
+          <MessageInput />
+        </div>
       </div>
     );
   }
---
+++ src/Components/Main/MessageInput.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MessageInput = () => {
+  return (
+    <div className="messageInputContainer" />
+  );
+}
+
+export default MessageInput;
