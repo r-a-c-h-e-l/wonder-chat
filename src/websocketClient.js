d719edf3e093faaa07c577bae0b4e3fb98507534
r-a-c-h-e-l <rahazes@gmail.com>
2018-12-03 08:56:54 -0500

    client socket cleanup

--- src/websocketClient.js
+++ src/websocketClient.js
@@ -1,14 +1,12 @@
 import {
   ADD_USER,
-  REMOVE_USER,
   ADD_MESSAGE,
   SET_USER_LIST,
 } from './Store/actionTypes';
-import { setUserList, setMessage, removeUser } from './Store/actionCreators';
+import { setUserList, setMessage } from './Store/actionCreators';
 
 const setupSocket = (dispatch, user) => {
   const socket = new WebSocket('ws://localhost:8989')
-  console.log({ user });
   socket.onopen = () => {
     socket.send(JSON.stringify({
       type: ADD_USER,
@@ -17,7 +15,6 @@ const setupSocket = (dispatch, user) => {
   }
   socket.onmessage = (event) => {
     const data = JSON.parse(event.data)
-    console.log("client data: ", data);
     switch (data.type) {
       case ADD_MESSAGE:
         const { message, authorId, authorName, id} = data.payload;
@@ -26,10 +23,6 @@ const setupSocket = (dispatch, user) => {
       case SET_USER_LIST:
         dispatch(setUserList(data.users))
         break
-      // case REMOVE_USER:
-      //   const staleUser = { ...data }
-      //   dispatch(removeUser(staleUser))
-      //   break
       default:
         break
     }
